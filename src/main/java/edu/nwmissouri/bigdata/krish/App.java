3393166cd97d6aa8e86bc4d20b13e6f1c8e3ac3b
Krishna-Koyyalamudi <krishna.kks3797@gmail.com>
2021-02-19 15:10:39 -0600

    Add initial Maven project files

---
+++ src/main/java/edu/nwmissouri/bigdata/krish/App.java
@@ -0,0 +1,13 @@
+package edu.nwmissouri.bigdata.krish;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello! This is Krishna Sumanth Koyyalamudi" );
+    }
+}
